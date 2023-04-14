f25e5d301fae5665039d17c1426d48281fe5a508
Hamid Taheri <htaheri@2-G-5.42heilbronn.de>
2023-04-14 18:16:39 +0200

    modified

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,9 +6,11 @@
 /*   By: htaheri <htaheri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 20:29:17 by htaheri           #+#    #+#             */
-/*   Updated: 2023/04/09 21:39:41 by htaheri          ###   ########.fr       */
+/*   Updated: 2023/04/14 18:16:18 by htaheri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isalpha(int c)
 {
