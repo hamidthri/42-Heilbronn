31ec27c778cb7809c83297e1c70ef2f694b02863
Hamid Taheri <htaheri@2-D-5.42heilbronn.de>
2023-05-12 16:22:51 +0200

    last version of get_next_line with score 125

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: htaheri <htaheri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/02 15:55:49 by htaheri           #+#    #+#             */
-/*   Updated: 2023/05/12 11:44:13 by htaheri          ###   ########.fr       */
+/*   Created: 2023/05/07 16:57:19 by htaheri           #+#    #+#             */
+/*   Updated: 2023/05/12 11:51:18 by htaheri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*gets_line(char *buffer)
 {
@@ -100,24 +100,24 @@ char	*fd_read(int fd, char *buffer)
 
 char	*get_next_line(int fd)
 {
-	static char		*buffer;
+	static char		*buffer[MAX_FD];
 	char			*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!buffer)
+	if (!buffer[fd])
 	{
-		buffer = (char *) ft_calloc(1, sizeof(char));
-		if (!buffer)
+		buffer[fd] = (char *) ft_calloc(1, sizeof(char));
+		if (!buffer[fd])
 			return (NULL);
 	}
-	buffer = fd_read(fd, buffer);
-	if (!buffer)
+	buffer[fd] = fd_read(fd, buffer[fd]);
+	if (!buffer[fd])
 	{
-		buffer = NULL;
+		buffer[fd] = NULL;
 		return (NULL);
 	}
-	line = gets_line(buffer);
-	buffer = next_line(buffer, line);
+	line = gets_line(buffer[fd]);
+	buffer[fd] = next_line(buffer[fd], line);
 	return (line);
 }
